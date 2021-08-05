9f6b675e57217682f427bc7d566f8815f6a355f3
Dave Larsen <kv0s.dave@gmail.com>
2021-08-05 14:22:01 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # commentToReadme
-Program to convert code comments in a Readme markdown file
+---
+This is a program will create a **Readme Markdown** file from the tagged comments and code in that source file.
+
+by Dave Larsen KV0S
+code at github.com/kv0s/
+
+Licensed under the GPL3
+---
---
+++ commandToReadme.go
@@ -0,0 +1,53 @@
+package main
+
+// # commentToReadme
+// ---
+// This is a program will create a **Readme Markdown** file from the tagged comments and code in that source file.
+//
+// by Dave Larsen KV0S
+// code at github.com/kv0s/
+//
+// Licensed under the GPL3
+// ---
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+const progname string = "commentToReadme"
+const version string = "0.0.1"
+
+func main() {
+	// fmt.Printf("%s Version %s\n", progname, version)
+
+	f, err := os.OpenFile("./commandToReadme.go", os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		log.Fatalf("open file error: %v", err)
+		return
+	}
+	defer f.Close()
+
+	rd := bufio.NewReader(f)
+	for {
+		line, err := rd.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			log.Fatalf("read file line error: %v", err)
+			return
+		}
+		if strings.Contains(line, "//") {
+			if strings.HasPrefix(line, "//") {
+				fmt.Printf("%s", strings.TrimPrefix(line, "//"))
+			}
+		}
+	}
+
+}
